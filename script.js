ffcdde2db2768567af58a911fd988e5eb932b06f
socrastein <31764357+socrastein@users.noreply.github.com>
2023-01-09 21:52:04 -0700

    Add Game controller functionality Newgame, character selection and ability to fill cells. Doesn't check for win conditions yet but alternates between allowing X & O selection on empty squares.

--- script.js
+++ script.js
@@ -1,5 +1,78 @@
+const Game = (() => {
+  var cpu = true;
+  var playerTurn = 1;
+  var p1Char = undefined;
+  var p2Char = undefined;
+
+  const newGame = function () {
+    console.log('Starting New Game');
+    playerTurn = 1;
+    p1Char = undefined;
+    p2Char = undefined;
+    gameBoard.clearBoard();
+    chooseChar();
+  }
+
+  const chooseChar = function () {
+    p1Char = undefined;
+    p2Char = undefined;
+
+    gameBoard.cells[0].style.borderStyle = "none";
+    gameBoard.cells[1].style.borderStyle = "none";
+    gameBoard.cells[2].style.borderStyle = "none";
+    gameBoard.cells[3].innerHTML = 'X';
+    gameBoard.cells[4].innerHTML = 'or';
+    gameBoard.cells[4].style.borderStyle = "none";
+    gameBoard.cells[5].innerHTML = 'O';
+    gameBoard.cells[6].style.borderStyle = "none";
+    gameBoard.cells[7].style.borderStyle = "none";
+    gameBoard.cells[8].style.borderStyle = "none";
+  }
+
+  const cellSelect = function (cell) {
+    if(p1Char === undefined){
+      if(cell === gameBoard.cells[3]){
+        p1Char = 'X';
+        p2Char = 'O';
+        gameBoard.clearBoard();
+        return
+      }
+      if(cell === gameBoard.cells[5]){
+        p1Char = 'O';
+        p2Char = 'X';
+        gameBoard.clearBoard();
+        return
+      }
+    }
+
+    if(cell.className.includes("clear")){
+      if(playerTurn === 1){
+        cell.innerHTML = p1Char;
+      } else cell.innerHTML = p2Char; 
+      switchTurn();
+      cell.setAttribute("class", "gameCell");
+    }
+
+  }
+
+  const switchTurn = function () {
+    if(playerTurn === 1){
+      playerTurn = 2;
+    } else playerTurn = 1;
+  }
+
+
+  return {newGame, cellSelect}
+})();
+
 const gameBoard = (() => {
+  const newGameButton = document.getElementById('newGame');
+  newGameButton.addEventListener('click', Game.newGame);
+
   const cells = [...document.querySelectorAll("button.gameCell")];
+  cells.forEach(cell => {
+    cell.addEventListener("click", () => Game.cellSelect(cell));
+  })
 
   const row1 = cells.slice(0,3);
   const row2 = cells.slice(3,6);
@@ -14,7 +87,7 @@ const gameBoard = (() => {
 
   const lines = [row1, row2, row3, col1, col2, col3, diag1, diag2];
 
-  const fillCell = function (cell, char) {
+  const fillCell = function (cell) {
     this.cells[cell].innerHTML = char;
   }
 
@@ -37,6 +110,8 @@ const gameBoard = (() => {
   const clearBoard = function () {
     cells.forEach(cell =>{
       cell.innerHTML = '';
+      cell.style.borderStyle = 'solid';
+      cell.setAttribute("class", "gameCell clear");
     })
   }
 
@@ -47,6 +122,12 @@ const gameBoard = (() => {
     diag1, diag2, lines,
     fillCell, checkWin, clearBoard};
 })();
+
+
+
+const player = function () {
+
+}
 
 console.log(gameBoard.checkWin());
 
